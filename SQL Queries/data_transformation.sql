cd2d8a63b3eaaab5edfe6f862a6cb4e5b5d2892f
Cesar <120736967+cjm1011@users.noreply.github.com>
2026-03-13 08:38:57 -0400

    data_transformation.sql

--- SQL Queries/data_transformation.sql
+++ SQL Queries/data_transformation.sql
@@ -1,20 +1,30 @@
 -- Split full name
 
-SELECT
-patient_id,
-SPLIT_PART(full_name,' ',1) AS first_name,
-SPLIT_PART(full_name,' ',2) AS last_name,
-TO_DATE(dob,'YYYY/MM/DD') AS birth_date,
-gender,
-phone
-FROM patients_legacy;
+--First, create the new columns in the table
+ALTER TABLE patients_new_system
+ADD COLUMN first_name TEXT,
+ADD COLUMN last_name TEXT;
+
+--Second, split the full_name column.
+UPDATE patients_new_system
+SET
+first_name = split_part(full_name, '_', 1),
+last_name = split_part(full_name, '_', 2);
+
+--lastly once the data is validated, drop the legacy column:
+ALTER TABLE patients_new_system
+DROP COLUMN full_name;
+
 
 --This ensures consistent phone number formatting for downstream systems.
-UPDATE patients_legacy
+UPDATE patients_new_system
 SET phone = REGEXP_REPLACE(phone, '[^0-9]', '', 'g');
 
 
 -- Handles missing phone number Values
-UPDATE patients_legacy
+UPDATE patients_new_system
 SET phone = '0000000000'
 WHERE phone IS NULL;
+
+
+--
