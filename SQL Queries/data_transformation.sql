b6e128abdbf9a5cfea7f76a7b2ffa5a6eb11aaa7
Cesar <120736967+cjm1011@users.noreply.github.com>
2026-03-13 09:56:33 -0400

    data_transformation.sql

--- SQL Queries/data_transformation.sql
+++ SQL Queries/data_transformation.sql
@@ -1,19 +1,19 @@
 -- Split full name
 
---First, create the new columns in the table
-ALTER TABLE patients_new_system
-ADD COLUMN first_name TEXT,
-ADD COLUMN last_name TEXT;
+      --First, create the new columns in the table
+      ALTER TABLE patients_new_system
+      ADD COLUMN first_name TEXT,
+      ADD COLUMN last_name TEXT;
 
---Second, split the full_name column.
-UPDATE patients_new_system
-SET
-first_name = split_part(full_name, '_', 1),
-last_name = split_part(full_name, '_', 2);
+      --Second, split the full_name column.
+      UPDATE patients_new_system
+      SET
+      first_name = split_part(full_name, '_', 1),
+      last_name = split_part(full_name, '_', 2);
 
---lastly once the data is validated, drop the legacy column:
-ALTER TABLE patients_new_system
-DROP COLUMN full_name;
+      --lastly once the data is validated, drop the legacy column:
+      ALTER TABLE patients_new_system
+      DROP COLUMN full_name;
 
 
 --This ensures consistent phone number formatting for downstream systems.
@@ -27,4 +27,4 @@ SET phone = '0000000000'
 WHERE phone IS NULL;
 
 
---
+
